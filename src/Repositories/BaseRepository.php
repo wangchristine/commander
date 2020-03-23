e9a4d9bba28b94a4fba0a352463c8a6dcdd4445d
wangchristine <c56984200123@gmail.com>
2020-03-23 23:43:14 +0800

    extends repository support function

--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -17,4 +17,37 @@ abstract class BaseRepository
         return $this->model->all($columns);
     }
 
+    /**
+     * Find a model by its primary key.
+     *
+     * @param  mixed  $id
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
+     */
+    public function find($id, $columns = ['*'])
+    {
+        return $this->model->find($id, $columns);
+    }
+
+    /**
+     * Save a new model and return the instance.
+     *
+     * @param  array  $attributes
+     * @return \Illuminate\Database\Eloquent\Model|$this
+     */
+    public function create(array $attributes = [])
+    {
+        return $this->model->create($attributes);
+    }
+
+    /**
+     * Begin querying a model with eager loading.
+     *
+     * @param  array|string  $relations
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function with($relations)
+    {
+        return $this->model->with($relations);
+    }
 }
